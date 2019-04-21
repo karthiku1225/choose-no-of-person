b0f157b0e324b5ae54261e90e3a667074b30d468
Karthik U <31045520+karthiku1225@users.noreply.github.com>
2019-04-22 03:42:33 +0530

    Add files via upload

---
+++ AngularInterview/README.md.txt
@@ -0,0 +1,43 @@
+Project Title
+Choose number of people in AngularJS
+
+Prerequisites
+Visual Studio 2019 or earlier, .net framework 7.2 or less
+AngularJS, bootstrap, font awesome
+
+Tasks
+1. Each of the + or - buttons can be clicked independent of the other
+2. At no point of time can an invalid combination of data be possible in the UI
+3. # Rooms can be minimum 1 and maximum 5
+4. # of persons in each room (i.e. total of Adults and Children) can be minimum 1 and
+maximum 4
+5. # of Adults can be >= 1 (i.e. a booking should have at least one adult)
+6. # of Children can be >= 0
+7. # of persons (Adults + Children) can be >= # of rooms (i.e. there should be at least one
+person per room)
+8. If Adult or Child count is increased, the room count can be increased, if required, to
+meet the constraint
+9. If Adult or Child count is decreased, the room count can be decreased, if required, to
+meet the constraint
+10. No count can automatically change unless required to meet the constraint
+11. If room count is increased, the adult count can be increased to the extent required to
+meet the constraintEach of the + or - buttons can be clicked independent of the other
+12. If room count is decreased, if # of persons exceed maximum allowed, first # of children
+can be reduced upto 0, if present; then # of adults can be reduced to the extent required
+to meet the constraint
+
+### Met all the conditions mentioned above. 
+
+Running the tests
+Tested manually
+
+Authors
+Karthik U
+
+License
+This project is licensed under the MIT License - see the LICENSE.md file for details
+
+Acknowledgments
+Hat tip to anyone whose code was used
+Inspiration
+etc
---
+++ AngularInterview/controllers/main.js
@@ -0,0 +1,368 @@
+﻿var myApp = angular.module("myApp", []);
+myApp.controller("AngularAssignment", ["$scope", AngularAssignment]);
+function AngularAssignment($scope) {
+    debugger
+    $scope.model = { Room: 1, Adult: 1, Child: 0 };
+    $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+    $scope.roomDec = function () {
+        $scope.disableRoomInc = false;
+        if ($scope.model.Room == 1) {
+            $scope.disableRoomDec = true;
+        }
+        else {
+            $scope.model.Room = $scope.model.Room - 1;
+        }
+        if ($scope.model.Room == 1) {
+            if ($scope.adultChildSum > 4) {
+                if ($scope.model.Child > 0) {
+                    while ($scope.adultChildSum > 4) {
+                        $scope.model.Child--;
+                        $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                        if ($scope.model.Child == 0)
+                            break;
+                    }
+                }
+                while ($scope.adultChildSum > 4) {
+                    $scope.model.Adult--;
+                    $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                }
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+        if ($scope.model.Room == 2) {
+            if ($scope.adultChildSum > 8) {
+                if ($scope.model.Child > 0) {
+                    while ($scope.adultChildSum > 8) {
+                        $scope.model.Child--;
+                        $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                        if ($scope.model.Child == 0)
+                            break;
+                    }
+                }
+                while ($scope.adultChildSum > 8) {
+                    $scope.model.Adult--;
+                    $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                }
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+        if ($scope.model.Room == 3) {
+            if ($scope.adultChildSum > 12) {
+                if ($scope.model.Child > 0) {
+                    while ($scope.adultChildSum > 12) {
+                        $scope.model.Child--;
+                        $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                        if ($scope.model.Child == 0)
+                            break;
+                    }
+                }
+                while ($scope.adultChildSum > 12) {
+                    $scope.model.Adult--;
+                    $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                }
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+        if ($scope.model.Room == 4) {
+            if ($scope.adultChildSum > 16) {
+                if ($scope.model.Child > 0) {
+                    while ($scope.adultChildSum > 16) {
+                        $scope.model.Child--;
+                        $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                        if ($scope.model.Child == 0)
+                            break;
+                    }
+                }
+               // if ($scope.adultChildSum > 16) {
+                    while ($scope.adultChildSum > 16) {
+                        $scope.model.Adult--;
+                        $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+                    }
+              //  }
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+    }
+    $scope.roomInc = function () {
+        $scope.disableRoomDec = false;
+        $scope.model.Room = $scope.model.Room + 1;
+        if (($scope.model.Room > 1 && ($scope.model.Adult+1 == $scope.model.Room))) {
+            $scope.model.Adult++;
+        }
+        if ($scope.model.Room == 5) {
+            $scope.disableRoomInc = true;
+        }
+        if ($scope.model.Adult > 1) {
+            $scope.disableAdultDec = false;
+        }
+        if ($scope.model.Child == 0) {
+            $scope.disableChildDec = true;
+        }
+        else {
+            $scope.disableChildDec = false;
+        }
+    }
+    $scope.adultDec = function () {
+        $scope.disableRoomInc = false;
+        if ($scope.model.Room >= 1) {
+            $scope.disableRoomDec = false;
+            //if ($scope.model.Adult > 1) {
+            //    $scope.model.Adult = $scope.model.Adult - 1;
+            //}
+        }
+        debugger
+        if ($scope.model.Room == 1) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            // }
+            if ($scope.adultChildSum > 4) {
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Adult > 1) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+            }
+            else {
+                $scope.disableAdultDec = true;
+            }
+        }
+        else if ($scope.model.Room == 2) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Adult == 2 && $scope.adultChildSum == 2) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Child > 2 && $scope.adultChildSum <=5) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Adult = $scope.model.Adult - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+
+        }
+        else if ($scope.model.Room == 3) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Adult == 3 && $scope.adultChildSum == 3) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Child >= 1 && $scope.adultChildSum <=5) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Adult = $scope.model.Adult - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+
+        else if ($scope.model.Room == 4) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Adult == 4 && $scope.adultChildSum == 4) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Child >= 1 && $scope.adultChildSum <=5) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Adult = $scope.model.Adult - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+        else if ($scope.model.Room == 5) {
+            $scope.disableRoomInc = true;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Adult == 5 && $scope.adultChildSum == 5) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Child >=1 && $scope.adultChildSum <= 5) {
+                $scope.model.Adult = $scope.model.Adult - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Adult = $scope.model.Adult - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+        if ($scope.model.Adult > 1) {
+            $scope.disableAdultDec = false;
+        } else {
+            $scope.disableAdultDec = true;
+        }
+    }
+    $scope.adultInc = function () {
+        $scope.disableRoomDec = false;
+        $scope.disableAdultDec = false;
+        if ($scope.model.Room == 1) {
+           // if ($scope.adultChildSum != 4) {
+            $scope.model.Adult = $scope.model.Adult + 1;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+           // }
+            if ($scope.adultChildSum > 4) {
+                $scope.model.Room++;
+            }
+        }
+        else if ($scope.model.Room == 2) {
+            $scope.model.Adult = $scope.model.Adult + 1;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 8) {
+                $scope.model.Room++;
+            }
+        }
+        else if ($scope.model.Room == 3) {
+            $scope.model.Adult = $scope.model.Adult + 1;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 12)
+                $scope.model.Room++;
+        }
+       
+        else if ($scope.model.Room == 4) {
+            if ($scope.adultChildSum != 20) {
+                $scope.model.Adult = $scope.model.Adult + 1;
+            }
+
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 16)
+                $scope.model.Room++;
+        }
+        else if ($scope.model.Room == 5) {
+            $scope.disableRoomInc = true;
+            if ($scope.adultChildSum != 20)
+                $scope.model.Adult = $scope.model.Adult + 1;
+        }
+
+        $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+    }
+    $scope.childDec = function () {
+        if ($scope.model.Room >= 1) {
+            $scope.disableRoomDec = false;
+        }
+
+        if ($scope.model.Room == 1) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 4) {
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Child > 0) {
+                $scope.model.Child = $scope.model.Child - 1;
+            }
+            else {
+                $scope.disableChildDec = true;
+            }
+        }
+        else if ($scope.model.Room == 2) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Child == 2 && $scope.adultChildSum == 2) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Adult >= 1 && $scope.adultChildSum <= 5) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Child = $scope.model.Child - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+
+        }
+        else if ($scope.model.Room == 3) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Child == 3 && $scope.adultChildSum == 3) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Adult >= 1 && $scope.adultChildSum <= 3) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Child = $scope.model.Child - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+
+        else if ($scope.model.Room == 4) {
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Child == 4 && $scope.adultChildSum == 4) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Adult >= 1 && $scope.adultChildSum <= 5) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Child = $scope.model.Child - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+        else if ($scope.model.Room == 5) {
+            $scope.disableRoomInc = true;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.model.Child == 5 && $scope.adultChildSum == 5) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else if ($scope.model.Adult >=1 && $scope.adultChildSum <= 5) {
+                $scope.model.Child = $scope.model.Child - 1;
+                $scope.model.Room--;
+            }
+            else {
+                $scope.model.Child = $scope.model.Child - 1;
+            }
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        }
+        if ($scope.model.Child > 0) {
+            $scope.disableChildDec = false;
+        } else {
+            $scope.disableChildDec = true;
+        }
+    }
+    $scope.childInc = function () {
+        $scope.disableRoomDec = false;
+        $scope.disableChildDec = false;
+        if ($scope.model.Room == 1) {
+            $scope.model.Child = $scope.model.Child + 1;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 4) {
+                $scope.model.Room++;
+            }
+        }
+        else if ($scope.model.Room == 2) {
+            $scope.model.Child = $scope.model.Child + 1;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 8) {
+                $scope.model.Room++;
+            }
+        }
+        else if ($scope.model.Room == 3) {
+            $scope.model.Child = $scope.model.Child + 1;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 12)
+                $scope.model.Room++;
+        }
+        else if ($scope.model.Room == 4) {
+            $scope.model.Child = $scope.model.Child + 1;
+            $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+            if ($scope.adultChildSum > 16)
+                $scope.model.Room++;
+        }
+        else if ($scope.model.Room == 5) {
+            $scope.disableRoomInc = true;
+            if ($scope.adultChildSum != 20)
+                $scope.model.Child = $scope.model.Child + 1;
+        }
+
+        $scope.adultChildSum = $scope.model.Adult + $scope.model.Child;
+        if ($scope.model.Adult > 1) {
+            $scope.disableAdultDec = false;
+        }
+        if ($scope.model.Child > 0) {
+            $scope.disableChildDec = false;
+        }
+    }
+}
